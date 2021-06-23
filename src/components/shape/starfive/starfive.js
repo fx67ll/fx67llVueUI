5a0421eac22d048dafa3f9897c4f02b9ba083c0b
fx67ll <2717745014@qq.com>
2021-06-23 18:00:52 +0800

    format name

--- src/components/shape/starfive/starfive.js
+++
@@ -1,8 +0,0 @@
-import shapeStarfive from './starfive.vue';
-shapeStarfive.install = function(Vue) {
-	Vue.component(shapeStarfive.name, shapeStarfive);
-};
-export {
-	default as shapeStarfive
-}
-from './starfive.vue'

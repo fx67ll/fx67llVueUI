347ad3b073c2c9c13652dd1f350f7fbbffbaf5b4
fx67ll <2717745014@qq.com>
2021-06-23 18:01:04 +0800

    format name

---
+++ src/components/shape/star-five/star-five.js
@@ -0,0 +1,8 @@
+import shapeStarFive from './star-five.vue';
+shapeStarFive.install = function(Vue) {
+	Vue.component(shapeStarFive.name, shapeStarFive);
+};
+export {
+	default as shapeStarFive
+}
+from './star-five.vue'
---
+++ src/components/shape/star-five/star-five.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="star-five-box" :style="{ '--shapeColor': shapeColor, '--shapeSize': shapeSize }"><div class="star-five"></div></div>
+</template>
+
+<script>
+export default {
+	name: 'shapeStarFive',
+	props: {
+		shapeColor: {
+			type: String,
+			required: false,
+			default: '#000000',
+			validator(color) {
+				return new RegExp('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$').test(color);
+			}
+		},
+		shapeSize: {
+			type: Number,
+			required: false,
+			default: 1,
+			validator(num) {
+				return new RegExp('^((-?)0|([1-9][0-9]*))(\.[0-9]+)?$').test(num);
+			}
+		}
+	}
+};
+</script>
+
+<style lang="less" scoped="scoped">
+.star-five-box {
+	width: 200px;
+	height: 180px;
+	overflow: hidden;
+	zoom: var(--shapeSize);
+	// 火狐不兼容zoom，用transform勉强代替
+	-moz-transform: scale(var(--shapeSize));
+	.star-five {
+		position: relative;
+		top: 58px;
+		left: 4px;
+		display: block;
+		color: var(--shapeColor);
+		width: 0px;
+		height: 0px;
+		border-right: 100px solid transparent;
+		border-bottom: 70px solid var(--shapeColor);
+		border-left: 100px solid transparent;
+		transform: rotate(35deg);
+	}
+	.star-five:before {
+		border-bottom: 80px solid var(--shapeColor);
+		border-left: 30px solid transparent;
+		border-right: 30px solid transparent;
+		position: absolute;
+		height: 0;
+		width: 0;
+		top: -45px;
+		left: -65px;
+		display: block;
+		content: '';
+		transform: rotate(-35deg);
+	}
+	.star-five:after {
+		position: absolute;
+		display: block;
+		color: var(--shapeColor);
+		top: 3px;
+		left: -105px;
+		width: 0px;
+		height: 0px;
+		border-right: 100px solid transparent;
+		border-bottom: 70px solid var(--shapeColor);
+		border-left: 100px solid transparent;
+		transform: rotate(-70deg);
+		content: '';
+	}
+}
+</style>
